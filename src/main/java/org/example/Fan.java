0f9d9c0794f48ea723e5dcd334831b747a114ccc
Disko <antoni20052016@gmail.com>
2023-12-14 23:17:11 +0300

    first commit

--- src/main/java/org/example/Fan.java
+++ src/main/java/org/example/Fan.java
@@ -3,21 +3,35 @@ package org.example;
 public class Fan {
     private final int bladeCount; // - количество лопастей
     private int mode = 1; // номер режима [1 - maxMode]
-    private final int maxMode; // количество доступных режимов
-    private final int potreblenieElektroenergii;
-    private int oborotovVMinutu = 2000; // 1500 + 500*mode
+    private int maxMode; // количество доступных режимов
+    private int potreblenieElektroenergii;
+    private int oborotovVMinutu = 2000;// 1500 + 500*mode
+    public int minut;
 
 
-    public Fan(int bladeCount, int maxMode, int potreblenieElektroenergii) {
+    public Fan(int bladeCount, int maxMode, int potreblenieElektroenergii, int minut) {
         // & && - и
         // | || - или
         if  (bladeCount > 0 & bladeCount < 100) {
             this.bladeCount = bladeCount;
         } else{
-            throw new RuntimeException("bladeCount should be > 0, current value = " + bladeCount);
+            throw new RuntimeException("bladeCount should be > 0 and < 100, current value = " + bladeCount);
         }
+        if (maxMode > 0 & maxMode < 10){
         this.maxMode = maxMode;
-        this.potreblenieElektroenergii = potreblenieElektroenergii;
+        } else{
+            throw new RuntimeException("maxMode should be > 0 and < 10, current value = " + maxMode);
+        }
+        if  ( potreblenieElektroenergii> 0 & potreblenieElektroenergii < 15) {
+            this.potreblenieElektroenergii = potreblenieElektroenergii;
+        } else{
+            throw new RuntimeException("potreblenieElektroenergii should be > 0 and < 15, current value = " + potreblenieElektroenergii);
+        }
+        if  (minut > 0 & minut < 100) {
+            this.minut = minut;
+        } else {
+            throw new RuntimeException("bladeCount should be > 0 and < 100, current value = " + bladeCount);
+        }
 
     }
 
@@ -34,5 +48,11 @@ public class Fan {
 //        }
         mode = maxMode - (maxMode - 1) * (mode - 1) % maxMode;
         oborotovVMinutu = 1500 + 500 * mode;
+    }
+    public  void printInfo(Fan fan) {
+        System.out.println("Количество оборотов: "+fan.oborotovVMinutu+" : " + " Режим: " + fan.mode+ "Потребление электроэнергии" + fan.potreblenieElektroenergii);
+    }
+    public  void printOborotovZaMinut(Fan fan){
+         System.out.println("Количество оборотов за" + fan.minut +":" + fan.minut * fan.oborotovVMinutu);
     }
 }
--- src/main/java/org/example/FanTest.java
+++ src/main/java/org/example/FanTest.java
@@ -3,21 +3,27 @@ package org.example;
 public class FanTest {
     // psvm + Tab
     public static void main(String[] args) {
-        Fan modelEkonom = new Fan(7, 3, 4); // создание нового экземпляра
+        Fan modelEkonom = new Fan(7, 3, 4, 3); // создание нового экземпляра
 
-        Fan modelStandart = new Fan(15, 5, 10);
+        Fan modelStandart = new Fan(15, 5, 10,5);
 //        modelStandart.bladeCount = 15;
 //        modelStandart.maxMode = 5;
 //        modelStandart.potreblenieElektroenergii = 10;
-
-//        printFanInfo(modelStandart);
+  //        printFanInfo(modelStandart);
 //        modelStandart.modeDown();
+
 
 
     }
 
-//    public static void printFanInfo(Fan fan) {
-//        System.out.println("количество оборотов: " + fan.oborotovVMinutu + " : "
-//                + " режим: " + fan.mode);
-//    }
+    //public static void printFanInfo(Fan fan) {
+    //System.out.println("количество оборотов: " + fan.oborotovVMinutu + " : "
+    //+ " режим: " + fan.mode);
+    //}
+    //public  void printInfo(Fan fan) {
+    //System.out.println("Количество оборотов: "+fan.oborotovVMinutu+" : " + " Режим: " + fan.mode+ "Потребление электроэнергии" + fan.potreblenieElektroenergii);
+   // }
+    //public  void printOborotovZaMinut(Fan fan){
+      //  System.out.println("Количество оборотов за" + minut +":" + minut * oborotovVMinutu);
+   // }
 }
